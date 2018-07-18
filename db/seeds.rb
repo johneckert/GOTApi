e4d6e9d1b8abd14169c07d29b7625447c27d0b6e
John Eckert <johnteckert@gmail.com>
2018-07-18 14:02:47 -0400

    created seed file and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'net/http'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#pgs 1-43
+page_num = 1
+while page_num < 44
+  res = Net::HTTP.get_response(URI("https://www.anapioficeandfire.com/api/characters?page=" + page_num.to_s + "&pageSize=50"))
+  page = JSON.parse(res.body)
+  page.each do |char|
+    #create new character instance
+    dead = char["died"].length > 0 ? true : false
+    c = Character.find_or_create_by(name: char["name"], gender: char["gender"], culture: char["culture"], dead: dead)
+  end
+  page_num += 1
+end
